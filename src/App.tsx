9716004d1ed89a84b7be343fab1bc2d3e6959b18
Eve <46225510+eveOfCode@users.noreply.github.com>
2019-05-07 17:14:14 -0300

    Update App.tsx - Final edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+/**
+ * Genevieve Twohig
+ * Date: 05/07/2019
+ * Version: 1.0
+ * Description: Testing file to render TimeInput component
+ */
+
 import React, { ComponentClass } from 'react';
 import TimeInput from "./TimeInput";
 
@@ -12,8 +19,8 @@ class ComposedTextField extends React.Component<IProps> {
     
     return (
       <div>
-        <TimeInput value=""
-        />
+        <h3>Time Input Field</h3>
+        <TimeInput value=""/>  
       </div>
     );
   }
